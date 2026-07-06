18d046e932fe744da6cc7055a6874416fbb8ec01
Kannan Kalidasan <kannanpoem1984@gmail.com>
2026-07-06 08:48:10 +0100

    updated the commit push

--- Formula/coral.rb
+++ Formula/coral.rb
@@ -7,16 +7,16 @@ class Coral < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kannandreams/coral/releases/download/v0.1.0/coral-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/kannandreams/coral/releases/download/v0.1.0/coral-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     url "https://github.com/kannandreams/coral/releases/download/v0.1.0/coral-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "PLACEHOLDER"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
