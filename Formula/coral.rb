753f2c546e97b7cd922bb77dcf2fcb1adbafddeb
Kannan Kalidasan <kannanpoem1984@gmail.com>
2026-07-06 09:07:04 +0100

    updated the sha

--- Formula/coral.rb
+++ Formula/coral.rb
@@ -7,16 +7,16 @@ class Coral < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kannandreams/coral/releases/download/v0.1.0/coral-aarch64-apple-darwin.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "06404d732ab00fef5f2192486eb55edecb675fc74bab346d73c080c7a5223b55"
     else
       url "https://github.com/kannandreams/coral/releases/download/v0.1.0/coral-x86_64-apple-darwin.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "2dac2c349d1d26934d13ebde8c22230e57e32508d9694a624bc43c4463d8124c"
     end
   end
 
   on_linux do
     url "https://github.com/kannandreams/coral/releases/download/v0.1.0/coral-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "7aa6f5ef3a4dda589ef320cd4b06a2314fa4d7512f583b5ab57e41a7dd19bec0"
   end
 
   def install
