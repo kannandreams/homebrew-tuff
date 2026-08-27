7445f4b056ecab97de586fd9310d28b396604261
Kannan Kalidasan <kannanpoem1984@gmail.com>
2026-08-27 09:25:06 +0100

    chore: update Tuff formula to 0.1.3

--- Formula/tuff.rb
+++ Formula/tuff.rb
@@ -1,22 +1,26 @@
 class Tuff < Formula
   desc "Capability lifecycle manager for coding agents"
   homepage "https://github.com/kannandreams/tuff"
+  version "0.1.3"
   license "MIT"
-  version "0.1.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/kannandreams/tuff/releases/download/v0.1.0/tuff-aarch64-apple-darwin.tar.gz"
-      sha256 "06404d732ab00fef5f2192486eb55edecb675fc74bab346d73c080c7a5223b55"
-    else
-      url "https://github.com/kannandreams/tuff/releases/download/v0.1.0/tuff-x86_64-apple-darwin.tar.gz"
-      sha256 "2dac2c349d1d26934d13ebde8c22230e57e32508d9694a624bc43c4463d8124c"
+    on_arm do
+      url "https://github.com/kannandreams/tuff/releases/download/v0.1.3/tuff-aarch64-apple-darwin.tar.gz"
+      sha256 "51eef467d8845c3a42aa34925099fd6a35b1fbd97ff69ea37cb812a8f47523b0"
+    end
+
+    on_intel do
+      url "https://github.com/kannandreams/tuff/releases/download/v0.1.3/tuff-x86_64-apple-darwin.tar.gz"
+      sha256 "b2123c3fd7c77963a3e7df808aedd2a5f6faf5ba53a9bf432917084aa890a3bb"
     end
   end
 
   on_linux do
-    url "https://github.com/kannandreams/tuff/releases/download/v0.1.0/tuff-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "7aa6f5ef3a4dda589ef320cd4b06a2314fa4d7512f583b5ab57e41a7dd19bec0"
+    on_intel do
+      url "https://github.com/kannandreams/tuff/releases/download/v0.1.3/tuff-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "57a9169363f61e58d2f2e50d400b0bc5a174e66104873d262fb3e178a539d405"
+    end
   end
 
   def install
